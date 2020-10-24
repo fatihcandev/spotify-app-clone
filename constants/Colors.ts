be73e52862769be29a42a5777104c8f12bfaa5a5
fatihcandev <fatihcan.social@gmail.com>
2020-10-24 11:24:39 +0300

    first commit

---
+++ constants/Colors.ts
@@ -0,0 +1,12 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+};
