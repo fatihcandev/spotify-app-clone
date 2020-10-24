77ed29015ea29ef49e843a5f5e09cea13eb2b261
fatihcandev <fatihcan.social@gmail.com>
2020-10-24 16:14:54 +0300

    refactor

---
+++ src/constants/Colors.ts
@@ -0,0 +1,9 @@
+export default {
+  green: "#1dd65e",
+  white: "#fff",
+  black: "#000",
+  lightGray: "#b4b4b4",
+  gray: "#454545",
+  darkGray: "#282828",
+  gradientPurple: "#5e2f59",
+};
---
+++ src/types.tsx
@@ -0,0 +1,36 @@
+export type RootStackParamList = {
+  Root: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Search: undefined;
+  Library: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type SearchParamList = {
+  SearchScreen: undefined;
+};
+
+export type LibraryParamList = {
+  LibraryScreen: undefined;
+};
+
+export enum IconName {
+  home = "home",
+  homeSolid = "homeSolid",
+  search = "search",
+  searchSolid = "searchSolid",
+  library = "library",
+  librarySolid = "librarySolid",
+  play = "play",
+  pause = "pause",
+  previous = "previous",
+  next = "next",
+  heart = "heart",
+  heartSolid = "heartSolid",
+}
