b68283af965005f503aab2b84ccf397a94c4d421
fatihcandev <fatihcan.social@gmail.com>
2020-11-15 00:01:40 +0300

    update

--- src/components/HomeCard/styles.ts
+++ src/components/HomeCard/styles.ts
@@ -4,28 +4,25 @@ import Colors from "../../constants/Colors";
 export default StyleSheet.create({
   container: {
     flexDirection: "row",
-    width: "48%",
+    width: "49%",
     height: 70,
-    maxHeight: 70,
     marginBottom: 15,
     backgroundColor: Colors.gray,
-    borderRadius: 8,
+    borderRadius: 8
   },
   textContainer: {
+    flex: 1,
     justifyContent: "center",
-    flexWrap: "wrap",
-    paddingLeft: 9,
+    paddingLeft: 9
   },
   text: {
     color: Colors.white,
     fontSize: 15,
-    fontWeight: "bold",
+    fontWeight: "bold"
   },
   image: {
     width: 70,
-    maxWidth: 70,
-    height: "auto",
     borderTopLeftRadius: 8,
-    borderBottomLeftRadius: 8,
-  },
+    borderBottomLeftRadius: 8
+  }
 });
