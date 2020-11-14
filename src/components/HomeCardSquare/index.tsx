326a0a18e84d50cc36659c9c84bb1b35c646f790
fatihcandev <fatihcan.social@gmail.com>
2020-11-15 00:01:51 +0300

    create home card square

---
+++ src/components/HomeCardSquare/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Text, Image, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+
+interface IHomeCardSquareProps {
+  image: any;
+  text: string;
+  onPress: () => void;
+}
+
+const HomeCardSquare: React.FC<IHomeCardSquareProps> = ({
+  image,
+  text,
+  onPress
+}) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <Image source={image} style={styles.image} />
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default HomeCardSquare;
---
+++ src/components/HomeCardSquare/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+import Colors from "../../constants/Colors";
+
+export default StyleSheet.create({
+  container: {
+    width: 150,
+    height: 200
+  },
+  image: {
+    width: "100%",
+    height: 150,
+    marginBottom: 10
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: Colors.white
+  }
+});
