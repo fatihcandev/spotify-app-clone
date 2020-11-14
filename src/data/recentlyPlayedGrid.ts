74e5bc2a220d9ad523870c369b1ae425f31737f8
fatihcandev <fatihcan.social@gmail.com>
2020-11-15 00:00:35 +0300

    refactor mock data

--- src/data/recentlyPlayedGrid.ts
+++ src/data/recentlyPlayedGrid.ts
@@ -1,40 +1,46 @@
 import { IHomeCardData } from "../types";
 
-export const homeCardData: IHomeCardData[] = [
+export const recentlyPlayedGridData: IHomeCardData[] = [
   {
+    id: "1",
     image:
       "https://daily-mix.scdn.co/covers/on_repeat/PZN_On_Repeat_DEFAULT-en.jpg",
     text: "On Repeat",
-    href: "OnRepeat",
+    href: "OnRepeat"
   },
   {
+    id: "2",
     image:
       "https://daily-mix.scdn.co/covers/backtracks/PZN_Repeat_Rewind_DEFAULT-en.jpg",
     text: "Repeat Rewind",
-    href: "RepeatRewind",
+    href: "RepeatRewind"
   },
   {
+    id: "3",
     image:
       "https://newjams-images.scdn.co/v2/discover-weekly/EJaHdCXNePwPVyRSEdqtU6gMtps0UcQgoONhLqbZdwjt2oQm2ylq3FVy8Il2x7VGG7Wr8cjmZGHedPrjE3Ls2DVM66S_AmVmgzRcxsBxDP4=/NTI6NjQ6NzFUMTMtMDEtMA==/default.jpg",
     text: "Discover Weekly",
-    href: "DiscoverWeekly",
+    href: "DiscoverWeekly"
   },
   {
+    id: "4",
     image:
       "https://daily-mix.scdn.co/covers/time-capsule/time-capsule-blue_DEFAULT-en.jpg",
     text: "Time Capsule",
-    href: "TimeCapsule",
+    href: "TimeCapsule"
   },
   {
+    id: "5",
     image:
       "https://dailymix-images.scdn.co/v2/img/865c453789287d6d52e59d84d8563c8c2c9d2efe/1/en/default.jpg",
     text: "Daily Mix 1",
-    href: "DailyMixOne",
+    href: "DailyMixOne"
   },
   {
+    id: "6",
     image:
       "https://dailymix-images.scdn.co/v2/img/ed0552e9746ed2bbf04ae4bcb5525700ca31522d/2/en/default.jpg",
     text: "Daily Mix 2",
-    href: "DailyMixTwo",
-  },
+    href: "DailyMixTwo"
+  }
 ];
---
+++ src/data/recentlyPlayedHorizontal.ts
@@ -0,0 +1,34 @@
+import { IHomeCardSquareData } from "../types";
+
+export const recentlyPlayedHorizontal: IHomeCardSquareData[] = [
+  {
+    id: "1",
+    cover: require("../../assets/images/lost-moon.jpg"),
+    name: "Lost Moon",
+    artist: "Nightlands"
+  },
+  {
+    id: "2",
+    cover: require("../../assets/images/planet-caravan.jpg"),
+    name: "Planet Caravan",
+    artist: "Black Sabbath"
+  },
+  {
+    id: "3",
+    cover: require("../../assets/images/desire-lines.jpg"),
+    name: "Desire Lines",
+    artist: "Lush"
+  },
+  {
+    id: "4",
+    cover: require("../../assets/images/primal.jpg"),
+    name: "Primal",
+    artist: "Slowdive"
+  },
+  {
+    id: "5",
+    cover: require("../../assets/images/rules.jpg"),
+    name: "Rules",
+    artist: "Khruangbin"
+  }
+];
