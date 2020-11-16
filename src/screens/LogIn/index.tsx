24889d9f22477507c3f31da7337de863763d4720
fatihcandev <fatihcan.social@gmail.com>
2020-11-16 16:21:43 +0300

    create login page

--- src/screens/LogIn/index.tsx
+++ src/screens/LogIn/index.tsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Keyboard, TouchableOpacity, View } from "react-native";
+
+import LogoAuth from "../../components/LogoAuth";
+import StyledButton from "../../components/StyledButton";
+import StyledInput from "../../components/StyledInput";
+import StyledText from "../../components/StyledText";
 
 import styles from "./styles";
 
 const LogIn: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleOutsideClick = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>LogIn</Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={handleOutsideClick}>
+      <LogoAuth />
+      <View style={styles.form}>
+        <StyledInput
+          onChange={v => setUsername(v)}
+          value={username}
+          placeholder="Username"
+          style={styles.input}
+          type="username"
+        />
+        <StyledInput
+          onChange={v => setPassword(v)}
+          value={password}
+          placeholder="Password"
+          style={styles.input}
+          type="password"
+        />
+        <StyledButton
+          variant="primary"
+          ariaLabel="log in"
+          onPress={() => {}}
+          style={styles.button}
+        >
+          LOG IN
+        </StyledButton>
+        <View style={styles.forgotTextContainer}>
+          <StyledText style={styles.forgotText}>Forgot password?</StyledText>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 };
 
--- src/screens/LogIn/styles.ts
+++ src/screens/LogIn/styles.ts
@@ -4,8 +4,28 @@ import Colors from "../../constants/Colors";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 20,
     backgroundColor: Colors.black
+  },
+  logo: {
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  input: {
+    marginBottom: 20
+  },
+  button: {
+    marginBottom: 20
+  },
+  forgotTextContainer: {
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  forgotText: {
+    color: Colors.lightGray
   }
 });
