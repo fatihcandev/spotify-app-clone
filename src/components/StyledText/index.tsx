6b814a483ff5c42b505529c1252aad4bc08dd914
fatihcandev <fatihcan.social@gmail.com>
2020-11-16 16:19:49 +0300

    create text component

---
+++ src/components/StyledText/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleProp, StyleSheet, Text, TextStyle } from "react-native";
+
+import styles from "./styles";
+
+interface IStyledTextProps {
+  bold?: boolean;
+  style?: StyleProp<any>;
+}
+
+const StyledText: React.FC<IStyledTextProps> = ({ bold, style, children }) => {
+  const getStyle = (): StyleProp<TextStyle> => {
+    if (bold) {
+      return { ...styles.text, fontWeight: "bold" };
+    } else {
+      return styles.text;
+    }
+  };
+
+  return <Text style={StyleSheet.compose(getStyle(), style)}>{children}</Text>;
+};
+
+export default StyledText;
---
+++ src/components/StyledText/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  text: {
+    fontSize: 16,
+    fontFamily: "Proxima Nova"
+  }
+});
